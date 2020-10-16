f673078532d584dbe0ba2c1e94f03866031f8df7
Sebastian Mocanu <diemetal851@gmail.com>
2020-10-16 03:24:01 +0300

    Updates

---
+++ precache-manifest.3ac8941e6a554a716a15e16323febd2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19088289159988b1f4deb6e102460467",
+    "url": "//code-test-app-site/index.html"
+  },
+  {
+    "revision": "406ce47227632eea0ef5",
+    "url": "//code-test-app-site/static/css/main.d551cbd2.chunk.css"
+  },
+  {
+    "revision": "54e2ec86c5864ad7c575",
+    "url": "//code-test-app-site/static/js/2.b362dae4.chunk.js"
+  },
+  {
+    "revision": "c13a2696425dc6973edb3eb4ea2e8804",
+    "url": "//code-test-app-site/static/js/2.b362dae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406ce47227632eea0ef5",
+    "url": "//code-test-app-site/static/js/main.ca6111ce.chunk.js"
+  },
+  {
+    "revision": "87cc3aa1a75dbd0f66b1",
+    "url": "//code-test-app-site/static/js/runtime-main.2c823e9a.js"
+  }
+]);
